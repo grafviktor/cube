48b26154846f1d57d19a73ed7948750f866853f9
Roman Leonenkov <6890447+grafviktor@users.noreply.github.com>
2025-10-12 16:55:29 +0100

    Add chapter 6

---
+++ ch06/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"time"
+
+	"cube/task"
+	"cube/worker"
+
+	"github.com/golang-collections/collections/queue"
+	"github.com/google/uuid"
+)
+
+const DEFAULT_HOST = "localhost"
+const DEFAULT_PORT = "5555"
+
+func main() {
+	host, defined := os.LookupEnv("CUBE_HOST")
+	if !defined {
+		log.Printf("CUBE_HOST not defined, falling back to default: %s", DEFAULT_HOST)
+		host = DEFAULT_HOST
+	}
+
+	portStr, defined := os.LookupEnv("CUBE_PORT")
+	if !defined {
+		log.Printf("CUBE_PORT not defined, falling back to default: %s", DEFAULT_PORT)
+		portStr = DEFAULT_PORT
+	}
+
+	port, err := strconv.Atoi(portStr)
+	if err != nil {
+		log.Fatalf("Invalid CUBE_PORT value: %v", err)
+	}
+
+	fmt.Println("Starting Cube worker")
+	w := worker.Worker{
+		Queue: *queue.New(),
+		Db:    make(map[uuid.UUID]*task.Task),
+	}
+	api := worker.Api{Address: host, Port: port, Worker: &w}
+	go runTasks(&w)
+	go w.CollectStats()
+	api.Start()
+}
+
+func runTasks(w *worker.Worker) {
+	for {
+		if w.Queue.Len() != 0 {
+			result := w.RunTask()
+			if result.Error != nil {
+				log.Printf("Error running task: %v\n", result.Error)
+			}
+		} else {
+			log.Printf("No tasks to process currently.\n")
+		}
+		log.Println("Sleeping for 10 seconds.")
+		time.Sleep(10 * time.Second)
+	}
+}
---
+++ ch06/task/task.go
@@ -0,0 +1,176 @@
+package task
+
+import (
+	"context"
+	"io"
+	"log"
+	"math"
+	"os"
+	"time"
+
+	"github.com/docker/go-connections/nat"
+	"github.com/google/uuid"
+	"github.com/moby/moby/api/pkg/stdcopy"
+	"github.com/moby/moby/api/types/container"
+	"github.com/moby/moby/client"
+)
+
+type Task struct {
+	ID            uuid.UUID
+	ContainerID   string
+	Name          string
+	State         State
+	Image         string
+	Cpu           float64
+	Memory        int64
+	Disk          int64
+	ExposedPorts  nat.PortSet
+	PortBindings  map[string]string
+	RestartPolicy container.RestartPolicyMode
+	StartTime     time.Time
+	FinishTime    time.Time
+}
+
+type TaskEvent struct {
+	ID        uuid.UUID
+	State     State
+	Timestamp time.Time
+	Task      Task
+}
+
+type Config struct {
+	// Name of the task, also used as the container name
+	Name string
+	// AttachStdin boolean which determines if stdin should be attached
+	AttachStdin bool
+	// AttachStdout boolean which determines if stdout should be attached
+	AttachStdout bool
+	// AttachStderr boolean which determines if stderr should be attached
+	AttachStderr bool
+	// ExposedPorts list of ports exposed
+	ExposedPorts nat.PortSet
+	// Cmd to be run inside container (optional)
+	Cmd []string
+	// Image used to run the container
+	Image string
+	// Cpu
+	Cpu float64
+	// Memory in MiB
+	Memory int64
+	// Disk in GiB
+	Disk int64
+	// Env variables
+	Env []string
+	// RestartPolicy for the container ["", "always", "unless-stopped", "on-failure"]
+	RestartPolicy container.RestartPolicyMode
+}
+
+func NewConfig(t *Task) *Config {
+	return &Config{
+		Name:          t.Name,
+		ExposedPorts:  t.ExposedPorts,
+		Image:         t.Image,
+		Cpu:           t.Cpu,
+		Memory:        t.Memory,
+		Disk:          t.Disk,
+		RestartPolicy: t.RestartPolicy,
+	}
+}
+
+type DockerResult struct {
+	Error       error
+	Action      string
+	ContainerId string
+	Result      string
+}
+
+type Docker struct {
+	Client *client.Client
+	Config Config
+}
+
+func NewDocker(c *Config) *Docker {
+	dc, _ := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	// If you use colima, use this line instead
+	// dc, _ := client.NewClientWithOpts(client.WithHost("unix:///Users/$username/.colima/docker.sock"), client.WithAPIVersionNegotiation())
+	return &Docker{
+		Client: dc,
+		Config: *c,
+	}
+}
+
+func (d *Docker) Run() DockerResult {
+	ctx := context.Background()
+	reader, err := d.Client.ImagePull(ctx, d.Config.Image, client.ImagePullOptions{})
+	if err != nil {
+		log.Printf("Error pulling image %s: %v\n", d.Config.Image, err)
+		return DockerResult{Error: err}
+	}
+	io.Copy(os.Stdout, reader)
+
+	rp := container.RestartPolicy{
+		Name: d.Config.RestartPolicy,
+	}
+
+	r := container.Resources{
+		Memory:   d.Config.Memory,
+		NanoCPUs: int64(d.Config.Cpu * math.Pow(10, 9)),
+	}
+
+	cc := container.Config{
+		Image:        d.Config.Image,
+		Tty:          false,
+		Env:          d.Config.Env,
+		ExposedPorts: d.Config.ExposedPorts,
+	}
+
+	hc := container.HostConfig{
+		RestartPolicy:   rp,
+		Resources:       r,
+		PublishAllPorts: true,
+	}
+
+	resp, err := d.Client.ContainerCreate(ctx, &cc, &hc, nil, nil, d.Config.Name)
+	if err != nil {
+		log.Printf("Error creating container using image %s: %v\n", d.Config.Image, err)
+		return DockerResult{Error: err}
+	}
+
+	err = d.Client.ContainerStart(ctx, resp.ID, client.ContainerStartOptions{})
+	if err != nil {
+		log.Printf("Error starting container %s: %v\n", resp.ID, err)
+		return DockerResult{Error: err}
+	}
+
+	out, err := d.Client.ContainerLogs(ctx, resp.ID, client.ContainerLogsOptions{ShowStdout: true, ShowStderr: true})
+	if err != nil {
+		log.Printf("Error getting logs for container %s: %v\n", resp.ID, err)
+		return DockerResult{Error: err}
+	}
+
+	stdcopy.StdCopy(os.Stdout, os.Stderr, out)
+
+	return DockerResult{ContainerId: resp.ID, Action: "start", Result: "success"}
+}
+
+func (d *Docker) Stop(id string) DockerResult {
+	log.Printf("Attempting to stop container %v", id)
+	ctx := context.Background()
+	err := d.Client.ContainerStop(ctx, id, client.ContainerStopOptions{Timeout: nil})
+	if err != nil {
+		log.Printf("Error stopping container %s: %v\n", id, err)
+		return DockerResult{Error: err}
+	}
+
+	err = d.Client.ContainerRemove(ctx, id, client.ContainerRemoveOptions{
+		RemoveVolumes: true,
+		RemoveLinks:   false,
+		Force:         false,
+	})
+	if err != nil {
+		log.Printf("Error removing container %s: %v\n", id, err)
+		return DockerResult{Error: err}
+	}
+
+	return DockerResult{Action: "stop", Result: "success", Error: nil}
+}
